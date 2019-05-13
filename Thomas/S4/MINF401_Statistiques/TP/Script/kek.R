330caf06420e31efb131246b6ed60228367f7232
Thomas Gigout <thomasgpro@hotmail.com>
2019-05-13 21:57:34 +0200

    iuhihu

--- Thomas/S4/MINF401_Statistiques/TP/Script/kek.R
+++ Thomas/S4/MINF401_Statistiques/TP/Script/kek.R
@@ -1,7 +1,8 @@
 n=1000
 mu=1
 sd=1
-Alpha =rnorm(n,mu,ds)
+Alpha = rnorm(n,mu,sd)
+Alpha
 hist(Alpha,freq=FALSE,col="purple",main="",ylab="GAAAAMMIIIIIIIIIIIIIIIIIIIIIIIIING",xlab="YA QUOI?!")
 X=seq(mu-3,mu+3,by=0.1)
 Y=dnorm(X,mu,sd)
